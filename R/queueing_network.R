c2f9cdef8c60f4e6155f6e46e08a03d33110b7d7
Basti@ThinkPad <sastibear@gmx.de>
2018-06-07 23:14:37 +0200

    Tests for main function included. Documentation for main function included.

--- R/queueing_network.R
+++ R/queueing_network.R
@@ -1,4 +1,22 @@
-### Main function creating 
+#' Simulate a Two-Node Queueing Network
+#' 
+#' Creates a simulation of a Poi/Geom/infty-Network with 2 nodes and routing 
+#' probabilities p_12 and p_21.
+#'
+#' @param n_obs Integer, number of observations to be analyzed
+#' @param burn_in Integer, length of burn in phase in order to get a stationary 
+#' distribution. Netowrk starts empty
+#' @param p_12 Numeric, in [0,1], routing probability from node 1 to node 2
+#' @param p_21 Numeric, in [0,1], routing probability from node 2 to node 1
+#' @param lambda_1 Numeric (> 0), (Poisson) arrival rate at node 1
+#' @param lambda_2 Numeric (> 0), (Poisson) arrival rate at node 2
+#' @param G_1 Numeric (in [0,1]), parameter steering the (Geometric) service 
+#' time distribution at node 1
+#' @param G_2 Numeric (in [0,1]), parameter steering the (Geometric) service 
+#' time distribution at node 2
+#' @return Data Frame with 4 columns (number of arrivals and departures) with 
+#' n_obs + burn_in rows, each representing one time slot.
+#' @export
 
 queueing_network_poi_geom <- 
   function(n_obs,
@@ -22,8 +40,8 @@ queueing_network_poi_geom <-
     progressbar <- txtProgressBar(style = 3)
     for(time in c(1:(n_obs + burn_in))){
       
-      arrival_1 = 2
-      arrival_2 = 0
+      arrival_1 = rpois(1, lambda = lambda_1)
+      arrival_2 = rpois(1, lambda = lambda_2)
       
       if(arrival_1 > 0){
         list_arrivals_1 = list_arrival(1, arrival_1, p_12 = p_12, p_21 = p_21, G_1 = G_1, G_2 = G_2)
@@ -100,31 +118,40 @@ fill_vector_with_zeros <- function(vector, lead, length){
 }
 
 simulate_single_customer <- function(enter_node, p_12, p_21, G_1, G_2){
-  number_of_steps = rgeom(1, (1-p_12*p_21))
-  if(enter_node == 1){
-    service_time_par_a = G_1
-    service_time_par_b = G_2
+  if(p_12 < 1 & p_12 >= 0 & p_21 < 1 & p_12 >= 0 & G_1 < 1 & G_1 >= 0 &  G_2 < 1 & G_2 >= 0){
+    number_of_steps = rgeom(1, (1-p_12*p_21))
+    if(enter_node == 1){
+      service_time_par_a = G_1
+      service_time_par_b = G_2
+    } else {
+      service_time_par_a = G_2
+      service_time_par_b = G_1
+    }
+    service_times <- rep(NA, number_of_steps + 1)
+    idx <- c(1:(number_of_steps +1))
+    service_times_a = rgeom(number_of_steps + 1, service_time_par_a)
+    service_times_b = rgeom(number_of_steps + 1, service_time_par_b)
+    service_times[idx %% 2 == 0] <- service_times_b[ idx %% 2 == 0] + 1
+    service_times[idx %% 2 != 0] <- service_times_a[ idx %% 2 != 0] + 1
+    return(service_times) 
   } else {
-    service_time_par_a = G_2
-    service_time_par_b = G_1
+    message("Probabilities of geometric distribution out of allowed range")
   }
-  service_times <- rep(NA, number_of_steps + 1)
-  idx <- c(1:(number_of_steps +1))
-  service_times_a = rgeom(number_of_steps + 1, service_time_par_a)
-  service_times_b = rgeom(number_of_steps + 1, service_time_par_b)
-  service_times[idx %% 2 == 0] <- service_times_b[ idx %% 2 == 0] + 1
-  service_times[idx %% 2 != 0] <- service_times_a[ idx %% 2 != 0] + 1
-  return(service_times)
 }
 
 customers_arrival <- function(enter_node, list_arrivals){
-  max_length = max(unlist(lapply(list_arrivals, sum)))
+  if(length(list_arrivals) > 0){
+    max_length = max(unlist(lapply(list_arrivals, sum)))
+  } else {max_length = 0}
   matrix_arrivals = 
     sapply(list_arrivals, 
            represent_single_customer, 
            simplify = TRUE, 
            enter_node = enter_node,
            max_length = max_length)
+  if(max_length == 0){
+    matrix_arrivals <- matrix("" , nrow = 0, ncol = 0)
+  }
   if(max_length == 1){
     matrix_arrivals <- as.matrix(t(matrix_arrivals))
   }
@@ -133,7 +160,7 @@ customers_arrival <- function(enter_node, list_arrivals){
 
 list_arrival <- function(enter_node, arrival, p_12, p_21, G_1, G_2){
   return(sapply(rep(enter_node, arrival), simplify = FALSE, simulate_single_customer,
-                           p_12 = p_12, p_21 = p_21, G_1 = G_1, G_2 = G_2))
+                p_12 = p_12, p_21 = p_21, G_1 = G_1, G_2 = G_2))
 }
 
 represent_single_customer <- function(enter_node, simulation, max_length){
--- tests/testthat/test-queueing_network.R
+++ tests/testthat/test-queueing_network.R
@@ -2,4 +2,49 @@ context("test-queueing_network.R")
 
 test_that("Result is Data Frame", {
   expect_equal(class(queueing_network_poi_geom(10,10,0.3, 0.4, 2, 1, 0.6, 0.7)),"data.frame")
+  expect_equal(class(queueing_network_poi_geom(1,1000,0.1, 0.9, 14, 2, 0.6, 0.7)),"data.frame")
+  expect_equal(class(queueing_network_poi_geom(13,4,0.99, 0.99, 1, 2, 0.6, 0.7)),"data.frame")
 })
+
+test_that("Result is Data Frame with 4 Columns", {
+  expect_equal(ncol(queueing_network_poi_geom(10,10,0.3, 0.4, 2, 1, 0.6, 0.7)),4)
+  expect_equal(ncol(queueing_network_poi_geom(1,1000,0.1, 0.9, 14, 2, 0.6, 0.7)),4)
+  expect_equal(ncol(queueing_network_poi_geom(13,4,0.99, 0.99, 1, 2, 0.6, 0.7)),4)
+})
+
+
+test_that("Result has predefined number of rows", {
+  expect_equal(nrow(queueing_network_poi_geom(10,10,0.3, 0.4, 2, 1, 0.6, 0.7)),20)
+  expect_equal(nrow(queueing_network_poi_geom(1,1000,0.1, 0.9, 14, 2, 0.6, 0.7)),1001)
+  expect_equal(nrow(queueing_network_poi_geom(13,4,0.99, 0.99, 1, 2, 0.6, 0.7)),17)
+})
+
+test_that("Number of exiting customers is almost that of entering customers", {
+  qn_1 <- queueing_network_poi_geom(10,10,0.3, 0.4, 2, 1, 0.6, 0.7)
+  qn_2 <- queueing_network_poi_geom(100,10,0.6, 0.1, 12, 1, 0.9, 0.7)
+  qn_3 <- queueing_network_poi_geom(10,100,0.1, 0.1, 0.06, 2, 0.1, 0.3)
+  
+  expect_equal(sum(qn_1$arrival_1 + qn_1$arrival_2) >  sum(qn_1$departure_1 + qn_1$departure_2), TRUE)
+  expect_equal(sum(qn_2$arrival_1 + qn_2$arrival_2) >  sum(qn_2$departure_1 + qn_2$departure_2), TRUE)
+  expect_equal(sum(qn_3$arrival_1 + qn_3$arrival_2) >  sum(qn_3$departure_1 + qn_3$departure_2), TRUE)
+  })
+
+test_that("Number of arriving customers is Poisson distributed", {
+  qn_1 <- queueing_network_poi_geom(10,10,0.3, 0.4, 2, 1, 0.6, 0.7)
+  qn_2 <- queueing_network_poi_geom(100,10,0.6, 0.1, 12, 1, 0.9, 0.7)
+  qn_3 <- queueing_network_poi_geom(10,100,0.1, 0.1, 0.06, 2, 0.1, 0.3)
+  
+  expect_equal(sum(qn_1$arrival_1) >  0.5*(10+10)*2, TRUE)
+  expect_equal(sum(qn_1$arrival_1) <  2*(10+10)*2, TRUE)
+  expect_equal(sum(qn_1$arrival_2) >  0.5*(10+10)*1, TRUE)
+  expect_equal(sum(qn_1$arrival_2) <  2*(10+10)*1, TRUE)
+  expect_equal(sum(qn_2$arrival_1) <  2*(100+10)*12, TRUE)
+  expect_equal(sum(qn_2$arrival_1) >  0.5*(100+10)*12, TRUE)
+  expect_equal(sum(qn_2$arrival_2) <  2*(100+10)*1, TRUE)
+  expect_equal(sum(qn_2$arrival_2) >  0.5*(100+10)*1, TRUE)
+  expect_equal(sum(qn_3$arrival_1) >  0.5*(100+10)*0.06, TRUE)
+  expect_equal(sum(qn_3$arrival_1) <  2*(100+10)*0.06, TRUE)
+  expect_equal(sum(qn_3$arrival_2) >  0.5*(100+10)*2, TRUE)
+  expect_equal(sum(qn_3$arrival_2) <  2*(100+10)*2, TRUE)
+})
+
