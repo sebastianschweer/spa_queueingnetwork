3137644aadd6bd6ec7f0a6670e4c3f31cf50a258
Sebastian Schweer <sebastian.schweer@1und1.de>
2018-06-07 08:59:47 +0200

    Modularized two functions. Tests yet to be written

--- R/queueing_network.R
+++ R/queueing_network.R
@@ -4,19 +4,19 @@ queueing_network_poi_geom <-
   function(n_obs,
            burn_in,
            p_12,
-	         p_21,
-			     lambda_1,
-			     lambda_2,
-			     G_1,
-			     G_2){
-
+           p_21,
+           lambda_1,
+           lambda_2,
+           G_1,
+           G_2){
+    
     ### Initiate the observations with 0
     observable_data <- 
       data.frame(arrival_1 = rep(0, n_obs + burn_in),
                  arrival_2 = rep(0, n_obs + burn_in),
                  departure_1 = rep(0, n_obs + burn_in),
                  departure_2 = rep(0, n_obs + burn_in))
-
+    
     ### Loop over each time point:
     ### 
     progressbar <- txtProgressBar(style = 3)
@@ -26,50 +26,47 @@ queueing_network_poi_geom <-
       arrival_2 = 0
       
       if(arrival_1 > 0){
-      customers_arrival_1 = 
-        customers_arrival(arrivals = arrival_1, enter_node = 1, 
-                          p_12 = p_12, p_21 = p_21, 
-                          G_1 = G_1, G_2 = G_2)
+        list_arrivals_1 = list_arrival(1, arrival_1, p_12 = p_12, p_21 = p_21, G_1 = G_1, G_2 = G_2)
         
-      
-      ### Departure Streams are appended with Departures resulting from new Arrivals
-      departures_enter_1_leave_1 = 
-        fill_vector_with_zeros(vector = rowSums(customers_arrival_1 == 1, na.rm = TRUE),
-                               lead = time,
-                               length = n_obs + burn_in)
-      
-      departures_enter_1_leave_2 = 
-        fill_vector_with_zeros(vector = rowSums(customers_arrival_1 == 2, na.rm = TRUE),
-                               lead = time,
-                               length = n_obs + burn_in)
-      
-      observable_data$departure_1 <- 
-        observable_data$departure_1 + departures_enter_1_leave_1
-      
-      observable_data$departure_2 <- 
-        observable_data$departure_2 + departures_enter_1_leave_2
+        customers_arrival_1 = customers_arrival(1, list_arrivals_1)
+        
+        ### Departure Streams are appended with Departures resulting from new Arrivals
+        departures_enter_1_leave_1 = 
+          fill_vector_with_zeros(vector = rowSums(customers_arrival_1 == 1, na.rm = TRUE),
+                                 lead = time,
+                                 length = n_obs + burn_in)
+        
+        departures_enter_1_leave_2 = 
+          fill_vector_with_zeros(vector = rowSums(customers_arrival_1 == 2, na.rm = TRUE),
+                                 lead = time,
+                                 length = n_obs + burn_in)
+        
+        observable_data$departure_1 <- 
+          observable_data$departure_1 + departures_enter_1_leave_1
+        
+        observable_data$departure_2 <- 
+          observable_data$departure_2 + departures_enter_1_leave_2
       }
       if(arrival_2 > 0){
-        customers_arrival_2 = 
-          customers_arrival(arrivals = arrival_2, enter_node = 2, 
-                            p_12 = p_12, p_21 = p_21, 
-                            G_1 = G_1, G_2 = G_2)
-      
+        list_arrivals_2 = list_arrival(2, arrival_2, p_12 = p_12, p_21 = p_21, G_1 = G_1, G_2 = G_2)
+        
+        customers_arrival_2 = customers_arrival(2, list_arrivals_2)
+        
         departures_enter_2_leave_1 = 
-        fill_vector_with_zeros(vector = rowSums(customers_arrival_2 == 1, na.rm = TRUE),
-                               lead = time,
-                               length = n_obs + burn_in)
-      
-      departures_enter_2_leave_2 = 
-        fill_vector_with_zeros(vector = rowSums(customers_arrival_2 == 2, na.rm = TRUE),
-                               lead = time,
-                               length = n_obs + burn_in)
-      
-      observable_data$departure_1 <- 
-        observable_data$departure_1 + departures_enter_2_leave_1
-      
-      observable_data$departure_2 <- 
-        observable_data$departure_2 + departures_enter_2_leave_2
+          fill_vector_with_zeros(vector = rowSums(customers_arrival_2 == 1, na.rm = TRUE),
+                                 lead = time,
+                                 length = n_obs + burn_in)
+        
+        departures_enter_2_leave_2 = 
+          fill_vector_with_zeros(vector = rowSums(customers_arrival_2 == 2, na.rm = TRUE),
+                                 lead = time,
+                                 length = n_obs + burn_in)
+        
+        observable_data$departure_1 <- 
+          observable_data$departure_1 + departures_enter_2_leave_1
+        
+        observable_data$departure_2 <- 
+          observable_data$departure_2 + departures_enter_2_leave_2
       }
       
       observable_data$arrival_1[time] <-
@@ -82,15 +79,15 @@ queueing_network_poi_geom <-
     }
     
     
-return(observable_data)
+    return(observable_data)
   }
 
 fill_vector_with_zeros <- function(vector, lead, length){
   if((length - length(vector) - lead) > 0){
-  result =
-    c(rep(0, lead), 
-      vector,
-      rep(0, (length - length(vector) - lead)))
+    result =
+      c(rep(0, lead), 
+        vector,
+        rep(0, (length - length(vector) - lead)))
   }
   else {
     result = c(rep(0, lead), vector)
@@ -98,9 +95,9 @@ fill_vector_with_zeros <- function(vector, lead, length){
   }
   return(
     suppressWarnings(
-    as.numeric(result)
+      as.numeric(result)
     ))
-  }
+}
 
 simulate_single_customer <- function(enter_node, p_12, p_21, G_1, G_2){
   number_of_steps = rgeom(1, (1-p_12*p_21))
@@ -120,9 +117,7 @@ simulate_single_customer <- function(enter_node, p_12, p_21, G_1, G_2){
   return(service_times)
 }
 
-customers_arrival <- function(arrivals, enter_node, p_12, p_21, G_1, G_2){
-  list_arrivals = sapply(rep(enter_node,arrivals), simplify = FALSE, simulate_single_customer,
-                         p_12 = p_12, p_21 = p_21, G_1 = G_1, G_2 = G_2)
+customers_arrival <- function(enter_node, list_arrivals){
   max_length = max(unlist(lapply(list_arrivals, sum)))
   matrix_arrivals = 
     sapply(list_arrivals, 
@@ -134,6 +129,11 @@ customers_arrival <- function(arrivals, enter_node, p_12, p_21, G_1, G_2){
     matrix_arrivals <- as.matrix(t(matrix_arrivals))
   }
   return(matrix_arrivals)
+}  
+
+list_arrival <- function(enter_node, arrival, p_12, p_21, G_1, G_2){
+  return(sapply(rep(enter_node, arrival), simplify = FALSE, simulate_single_customer,
+                           p_12 = p_12, p_21 = p_21, G_1 = G_1, G_2 = G_2))
 }
 
 represent_single_customer <- function(enter_node, simulation, max_length){
@@ -152,7 +152,7 @@ represent_single_customer <- function(enter_node, simulation, max_length){
   }
   represent = rep(NA, max_length)
   if(sum(simulation) <= max_length){
-  represent[sum(simulation)] = exit_node
+    represent[sum(simulation)] = exit_node
   }
   return(represent)
 }
--- tests/testthat/test-simulate_single_customer.R
+++ tests/testthat/test-simulate_single_customer.R
@@ -1,5 +1,12 @@
 context("test-simulate_single_customer.R")
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Result is numeric vector", {
+  expect_equal(class(simulate_single_customer(1,0.9,0.9,0.3,0.7)), "numeric")
+  expect_equal(class(simulate_single_customer(2,0.1,0.001,0.3,0.7)), "numeric")
+  expect_equal(class(simulate_single_customer(2,0.001,0.001,0.3,0.7)), "numeric")
+  expect_equal(class(simulate_single_customer(1,1,1,0.3,0.7)), "numeric")
+  expect_equal(is.vector(simulate_single_customer(1,0.9,0.9,0.3,0.7)), TRUE)
+  expect_equal(is.vector(simulate_single_customer(2,0.1,0.001,0.3,0.7)), TRUE)
+  expect_equal(is.vector(simulate_single_customer(2,0.001,0.001,0.3,0.7)), TRUE)
+  expect_equal(is.vector(simulate_single_customer(1,1,1,0.3,0.7)), TRUE)
 })
